97f01a997900ee12d6abcdec35ca084bb661155b
Olivia DeMoss <odemoss@goodwillworks.org>
2021-10-12 14:07:08 -0400

    update

--- app1.js
+++ app1.js
@@ -1,30 +1,46 @@
 /* Write a message to the console that says "Hello world!" */
 
-console.log ("Hello world!")
+    console.log ("Hello world!");
 
 /* Write a warning to the console that says "This is a warning!" */
 
-console.warn ("This is a warning!")
+    console.warn ("This is a warning!");
 
 /*  8) Write an error to the console that says "Error! System critical!" */
 
+    console.error ("Error! System critical!");
+
 /* 9) Declare a variable named var1 */
+
 
 /* 10) Assign a string data type value to var1 */
 
+let var1
+var1 = "stringvar1";
+
 /* 11) Write the value of var1 to the console */
+
+console.log("var1");
 
 /* 12) Write a blank line to the console. 
 (I showed one way to do this in class, there are 
 some others feel free to use those instead if you'd like, practice your google-fu) */
 
+console.log("\n");
+
 /* 13) Define and assign a variable named var2, the value should be of the number type */
+let var2 
+var2 = "2";
 
 /* 14) Evaluate var1 + var2 and write the result to the console 
 (You can store the result in a variable and write it to the console, or 
-    pass the expression into a method that will remain un-named)*/
+pass the expression into a method that will remain un-named)*/
 
-/* 15) Could you guess the result? Write your guess in an inline comment next to where you're writing it to the console */
+console.log("var1" + "var2");
+
+// 15) Could you guess the result? Write your guess in an inline comment next to where you're writing it to the console //
+
+// var1 and 2?
 
 /* 16) Add in a multi line comment and write some of the types we covered in class today on individual lines */
 
@@ -44,4 +60,4 @@ some others feel free to use those instead if you'd like, practice your google-f
 
 /* 23) Reassign var2 to the string 5 */
 
-/* 24) In any order or in however many lines you'd like, parse var2 from a string to a number and add it to var1, write your result to the console.
+/* 24) In any order or in however many lines you'd like, parse var2 from a string to a number and add it to var1, write your result to the console. */
