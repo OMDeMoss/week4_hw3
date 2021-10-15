686b4b5bcd70c06ae016363807a3ea4edc374978
Olivia DeMoss <odemoss@goodwillworks.org>
2021-10-15 15:26:45 -0400

    updates

---
+++ app2.js
@@ -0,0 +1,36 @@
+//a) The prompt should ask the user "Enter a number"
+
+
+var num1 = prompt('Enter a Number')
+console.log(num1);
+
+
+
+// b) Store the result in a variable named num1(you can use different names if you'd like)
+
+// 5) Using the prompt method again ask the user for some more input
+
+
+// a) The prompt should ask the user "Enter another number"
+// b) Store the result in a variable named num2
+
+// 6) Alert the user to the solution of num1 to the power of num2
+
+// 7) Generate a random number between 1 & 3, write the result to the console
+
+// 8) Create an array named alphabet
+// a) The elements in the array should be "a", "b", "c", "d", "e", "f"
+
+ // 9) Remove "f" from our alphabet array
+
+// 10) Add "g" on the end of our array
+
+// 11) Remove "a" from our alphabet array
+
+// 12) Add the number 1 to the front of our array
+
+// 13) Write the entire alphabet array to the console, you should see[ 1, "b", "c", "d", "e", "g" ] if you don't go back and check your work before moving on
+
+// 14) Push your work to github
+
+// 15) Merge you practice branch into master / main
