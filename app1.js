d010988dc4c98fcc59a981fd0b3f6ddd5c0c33bf
Olivia DeMoss <odemoss@goodwillworks.org>
2021-10-18 13:48:33 -0400

    update

--- app1.js
+++ app1.js
@@ -1,22 +1,20 @@
-/* Write a message to the console that says "Hello world!" */
+// Write a message to the console that says "Hello world!" */
 
     console.log ("Hello world!");
 
-/* Write a warning to the console that says "This is a warning!" */
+// Write a warning to the console that says "This is a warning!" */
 
     console.warn ("This is a warning!");
 
-/*  8) Write an error to the console that says "Error! System critical!" */
+//  8) Write an error to the console that says "Error! System critical!" */
 
     console.error ("Error! System critical!");
 
-/* 9) Declare a variable named var1 */
+// 9) Declare a variable named var1 */
 
+// 10) Assign a string data type value to var1 */
 
-/* 10) Assign a string data type value to var1 */
-
-let var1
-var1 = "stringvar1";
+var var1 = ('Value')
 
 /* 11) Write the value of var1 to the console */
 
@@ -29,8 +27,7 @@ some others feel free to use those instead if you'd like, practice your google-f
 console.log("\n");
 
 /* 13) Define and assign a variable named var2, the value should be of the number type */
-let var2 
-var2 = "2";
+var var2 =2;
 
 /* 14) Evaluate var1 + var2 and write the result to the console 
 (You can store the result in a variable and write it to the console, or 
@@ -38,26 +35,34 @@ pass the expression into a method that will remain un-named)*/
 
 console.log("var1" + "var2");
 
-// 15) Could you guess the result? Write your guess in an inline comment next to where you're writing it to the console //
+// 15) Could you guess the result? Write your guess in an inline comment next to where you're writing it to the console var1 and 2?
 
-// var1 and 2?
+// 16) Add in a multi line comment and write some of the types we covered in class today on individual lines */
 
-/* 16) Add in a multi line comment and write some of the types we covered in class today on individual lines */
+// 17) Write the type of the var1 variable to the console */
 
-/* 17) Write the type of the var1 variable to the console */
+// 18) Can you guess the result? Write your guess in an inline comment next to where you're writing to the console.*/
 
-/* 18) Can you guess the result? Write your guess in an inline comment next to where you're writing to the console.*/
+// 19) Declare a variable named var3 and assign it the string value text */
+var var3 = ('text');
 
-/* 19) Declare a variable named var3 and assign it the string value text */
+// 20) Write the value of var3 to the console */
+console.log("var3")
 
-/* 20) Write the value of var3 to the console */
+// 20) Reassign var3 to a boolean value */
 
-/* 20) Reassign var3 to a boolean value */
+var3 = 0;
 
-/* 21) Write the value of var3 to the console */
+// 21) Write the value of var3 to the console */
+console.log(Boolean (var3))
 
-/* 22) Reassign var1 to the number 5 */
+// 22) Reassign var1 to the number 5 */
+var1 = 5;
 
 /* 23) Reassign var2 to the string 5 */
 
+var2 = ('3');
+
 /* 24) In any order or in however many lines you'd like, parse var2 from a string to a number and add it to var1, write your result to the console. */
+
+console.log(parseInt("var2") + "var1")
